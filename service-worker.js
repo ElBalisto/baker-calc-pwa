ffb748a4c3dc658f902425c52770d23a38def7df
ElBalisto <79667483+ElBalisto@users.noreply.github.com>
2026-02-26 13:28:40 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE = 'baker-calc-inline-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method === 'GET' && new URL(req.url).origin === self.location.origin) {
+    event.respondWith(
+      caches.match(req).then(cached => cached || fetch(req).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put(req, copy));
+        return resp;
+      }))
+    );
+  }
+});
